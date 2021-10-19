134095e5c20e894d83e1cacdcc6386a85fa0e453
Leandro <officialdevmi@gmail.com>
2021-10-19 06:04:31 -0700

    Add calculator structure

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,7 +8,40 @@ class Calculator extends React.Component {
 
   render() {
     return (
-      <div>Calculator</div>
+      <table className="TableContainer">
+        <tr>
+          <td className="TableCell Input" colSpan="4">0</td>
+        </tr>
+        <tr>
+          <td className="TableCell">AC</td>
+          <td className="TableCell">+/-</td>
+          <td className="TableCell">%</td>
+          <td className="TableCell Orange">:</td>
+        </tr>
+        <tr>
+          <td className="TableCell">7</td>
+          <td className="TableCell">8</td>
+          <td className="TableCell">9</td>
+          <td className="TableCell Orange">x</td>
+        </tr>
+        <tr>
+          <td className="TableCell">4</td>
+          <td className="TableCell">5</td>
+          <td className="TableCell">6</td>
+          <td className="TableCell Orange">-</td>
+        </tr>
+        <tr>
+          <td className="TableCell">1</td>
+          <td className="TableCell">2</td>
+          <td className="TableCell">3</td>
+          <td className="TableCell Orange">+</td>
+        </tr>
+        <tr>
+          <td className="TableCell" colSpan="2">0</td>
+          <td className="TableCell">.</td>
+          <td className="TableCell Orange">=</td>
+        </tr>
+      </table>
     );
   }
 }
